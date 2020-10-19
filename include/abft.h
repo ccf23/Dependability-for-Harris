c6d832f615833631a1fbbed08beafac77254fff4
Daniel Stumpp <dcs98@pitt.edu>
2020-10-18 23:35:17 -0400

    added abft functions

---
+++ include/abft.h
@@ -0,0 +1,28 @@
+#ifndef ABFT_H
+#define ABFT_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+using namespace std;
+using namespace cv;
+
+typedef enum
+{
+    NONE,
+    GRAYSCALE
+} abftMode;
+/*
+struct abftMat
+{
+    Mat img;
+    bool hasRowSum;
+    bool hasColSum;
+
+    abftMat(Mat m, abftMode mode);
+    //Mat doGrayscaleABFT(Mat);
+    Mat encode(Mat, abftMode);
+};*/
+
+Mat doGrayscaleABFT(Mat);
+
+#endif
---
+++ src/abft.cpp
@@ -0,0 +1,86 @@
+
+#include "../include/abft.h"
+/*
+abftMat::abftMat(Mat m, abftMode mode)
+{
+    /*
+    switch (mode)
+    {
+    case NONE:
+        img = m;
+        hasRowSum = false;
+        hasColSum = false;
+        break;
+    case GRAYSCALE:
+        Mat im = m;
+        img = doGrayscaleABFT(im);
+        break;
+    default:
+
+        break;
+    }*/
+    
+//}//*/
+/*
+Mat abftMat::encode(Mat m, abftMode mode)
+{
+    switch (mode)
+    {
+    case NONE:
+        img = m;
+        hasRowSum = false;
+        hasColSum = false;
+        break;
+    case GRAYSCALE:
+        img = doGrayscaleABFT(m);
+        break;
+    default:
+
+        break;
+    }
+    return img;
+}
+*/
+
+Mat doGrayscaleABFT(Mat im)
+{
+    int rows = im.rows;
+    int cols = im.cols;
+
+    Mat rSum = Mat::zeros(rows, 1, CV_8UC3);    
+    Mat cSum = Mat::zeros(cols + 1, 1, CV_8UC3);
+
+    float cs0, cs1, cs2;    
+    for (int r = 0; r < rows; ++r)
+    {
+        cs0 = cs1 = cs2 = 0;
+        for (int i = 0; i < cols; ++i)
+        {
+            cs0 += im.at<Vec3b>(r,i)[0];
+            cs1 += im.at<Vec3b>(r,i)[1];
+            cs2 += im.at<Vec3b>(r,i)[2];
+        }
+        //cout<<r<<endl;
+        rSum.at<Vec3b>(r,1)[0] = cs0;
+        rSum.at<Vec3b>(r,1)[1] = cs1;
+        rSum.at<Vec3b>(r,1)[2] = cs2;
+    }
+    
+    for (int c = 0; c < cols; ++c)
+    {
+        cs0 = cs1 = cs2 = 0;
+        for (int i = 0; i < rows; ++i)
+        {
+            cs0 += im.at<Vec3b>(i,c)[0];
+            cs1 += im.at<Vec3b>(i,c)[1];
+            cs2 += im.at<Vec3b>(i,c)[2];
+        }
+        cSum.at<Vec3b>(c,1)[0] = cs0;
+        cSum.at<Vec3b>(c,1)[1] = cs1;
+        cSum.at<Vec3b>(c,1)[2] = cs2;
+    } 
+    
+    hconcat(im, rSum, im);
+    vconcat(im, cSum.t(), im);
+    return im;
+}
