559c6dbe0692396bb304a5ee478d8e25a00425b5
Daniel Stumpp <dcs98@pitt.edu>
2020-11-27 23:34:35 -0500

    moved featureStats struct into processing header

--- include/processing.h
+++ include/processing.h
@@ -3,6 +3,14 @@
 
 #include "util.h"
 
+struct featureStats {
+	uint missing_features; // number of features in benchmark not found in test
+	uint false_features;   // number of features found in test that don't have match in benchmark
+	uint bench_features;   // number of features in benchmark
+	uint test_features;    // number of features found in test
+	uint match_features;   // number of features that match between benchmark and test
+};
+
 class processing
 {
     public:
